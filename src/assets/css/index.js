5255e5ebd1cfddbfef90777bbf48699433068e4d
GM-atteoni <pepematteoni@hotmail.com>
2020-01-21 12:50:45 -0300

    inicio integração back com front

--- src/assets/css/index.js
+++ src/assets/css/index.js
@@ -1,3 +1,27 @@
+let arrPosts = [];
+
 function closeFooter(){
     document.getElementById('footer-popup-news').style.display = 'none';
+}
+
+function getPosts(){
+    const UTL_TO_FETCH = 'https://api-blog-gama.herokuapp.com/posts';
+
+    fetch(UTL_TO_FETCH, {
+        method: 'get'
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        populaBlog(result[0]);
+    })
+    .catch((error) => {
+        console.log(error.json());
+    })
+}
+
+function populaBlog(result){
+    let containerPrincipal = document.getElementById('principal-new');
+    console.log(result);
+    containerPrincipal.getElementsByTagName('h1')[0].innerText = result.titulo;
+    containerPrincipal.getElementsByTagName('p')[0].innerText = result.corpo;
 }
