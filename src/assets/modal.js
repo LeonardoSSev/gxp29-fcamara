57e6a80931deabbfd9e059d8dd45921d4e33a22b
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-24 00:24:44 -0300

    Adds functions for declining/accepting the terms

--- src/assets/modal.js
+++ src/assets/modal.js
@@ -1,11 +1,23 @@
+let isTermosAceitos = false;
+
 let modal = document.getElementById("myModal");
 
 let span = document.getElementsByClassName("avengers-modal-close")[0];
+
 
 function abrirTermos() {
   modal.style.display = "block";
 }
 
+function aceitarTermos() {
+  isTermosAceitos = true;
+  fecharTermos();
+}
+
+function recusarTermos() {
+  isTermosAceitos = false;
+  fecharTermos();
+}
 function fecharTermos() {
   modal.style.display = "none";
 }
