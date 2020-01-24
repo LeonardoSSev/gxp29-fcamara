c152552f0eb21a4467a90e52f6f20fe8ccbbc7e5
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-23 23:42:14 -0300

    Adds the modal's controls

---
+++ src/assets/modal.js
@@ -0,0 +1,11 @@
+let modal = document.getElementById("myModal");
+
+let span = document.getElementsByClassName("close")[0];
+
+function abrirTermos() {
+  modal.style.display = "block";
+}
+
+function fecharTermos() {
+  modal.style.display = "none";
+}
